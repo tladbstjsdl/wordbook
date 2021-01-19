58096dad25e009623da0e548a2ac5b9d78a55e75
TrainingDummy1 <44523400+tladbstjsdl@users.noreply.github.com>
2021-01-19 23:42:48 +0900

    Delete src/main/kotlin/com/dummy/wordbook/member/form directory

--- src/main/kotlin/com/dummy/wordbook/member/form/MemberForm.kt
+++
@@ -1,9 +0,0 @@
-package com.dummy.wordbook.member.form
-
-class MemberForm {
-	var memberId: String? = null
-	var password: String? = null
-	var phone: String? = null
-	var email: String? = null
-	var address: String? = null
-}
