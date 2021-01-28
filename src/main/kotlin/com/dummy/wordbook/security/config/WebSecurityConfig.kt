6b7ac2a017a35f5dd46ca52f1b68d14ac89f5c5a
TrainingDummy1 <f_faker@naver.com>
2021-01-29 03:26:03 +0900

    Spring Security Processing...

---
+++ src/main/kotlin/com/dummy/wordbook/security/config/WebSecurityConfig.kt
@@ -0,0 +1,47 @@
+package com.dummy.wordbook.security.config
+
+import com.dummy.wordbook.member.service.MemberService
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.builders.WebSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+
+@Configuration
+@EnableWebSecurity
+class WebSecurityConfig(private val memberService: MemberService) : WebSecurityConfigurerAdapter() {
+	@Bean
+	public fun passwordEncoder(): PasswordEncoder {
+		return BCryptPasswordEncoder()
+	}
+
+	override fun configure(web: WebSecurity?) {
+		web?.ignoring()?.requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+	}
+
+	override fun configure(http: HttpSecurity?) {
+		http?.csrf()?.disable()?.authorizeRequests()
+			?.antMatchers("/", "/insertForm")?.permitAll()
+			?.antMatchers("/wordbook", "/wordbooklist")?.authenticated()
+			?.antMatchers("/notice/insert")?.hasRole("admin")
+			?.and()
+		?.formLogin()
+			?.loginPage("/")
+			?.defaultSuccessUrl("/")
+			?.permitAll()
+			?.usernameParameter("memberId")
+			?.and()
+		?.logout()
+			?.invalidateHttpSession(true)
+	}
+
+	override fun configure(auth: AuthenticationManagerBuilder?) {
+		auth?.userDetailsService(memberService)?.passwordEncoder(passwordEncoder())
+	}
+
+}
